a7a4b5a3cc902b136448e3472910dbafc5d8ac5d
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-15 06:53:10 +1000

    :sparkles:Added EventDetail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MasterEventsListPageComponent } from './pages/master-events-list-page/master-events-list-page.component';
 import { MapViewPageComponent } from './pages/map-view-page/map-view-page.component';
+import { EventDetailComponent } from './components/event-detail/event-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MasterEventsListPageComponent,
-    MapViewPageComponent
+    MapViewPageComponent,
+    EventDetailComponent
   ],
   imports: [
     BrowserModule,
