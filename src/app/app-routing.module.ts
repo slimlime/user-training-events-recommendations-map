9a6c77d690d0f7af743de22ac828071642a93a7b
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-15 06:56:59 +1000

    Routing to MasterEventsListPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
+import { MasterEventsListPageComponent } from './pages/master-events-list-page/master-events-list-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  // Default redirect to MasterEventsListPage
+  {
+    path: '',
+    redirectTo: '/events',
+    pathMatch: 'full'
+  },
+  {
+    path: 'events',
+    component: MasterEventsListPageComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
