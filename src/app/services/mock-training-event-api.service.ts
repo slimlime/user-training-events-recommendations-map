ffad9404078ee389e1e28b8f5784e13ab0fa0afa
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-15 07:13:06 +1000

    :construction: Outlining TrainingEvent API functions

--- src/app/services/mock-training-event-api.service.ts
+++ src/app/services/mock-training-event-api.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
 
+import { TrainingEvent } from './../models/training-event';
+import { DATA_SEED_TRAINING_EVENTS } from './../seed/seed-training-events';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MockTrainingEventApiService {
 
   constructor() { }
+
+  getEvents() {
+
+  }
+
+  /**
+   * Default next five events
+   *
+   * @param {number} [numEvents=5]
+   * @returns {TrainingEvent[]}
+   * @memberof MockTrainingEventApiService
+   */
+  getMostRecentEvents(numEvents: number = 5): TrainingEvent[] {
+    const seedTestData = DATA_SEED_TRAINING_EVENTS;
+
+    return seedTestData;
+  }
+
+  sortEventsDateTime() {
+    // use Array.sort() with custom function
+  }
+
+  compareUpcomingEventSoonest() {
+
+  }
+
+  compareUpcomingEventLatest() {
+
+  }
+
 }
