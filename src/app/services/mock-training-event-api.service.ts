aea3fd45f79a077822661dc99d148b19874e01bb
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-15 08:22:27 +1000

    :bulb: Documented intended functionality

--- src/app/services/mock-training-event-api.service.ts
+++ src/app/services/mock-training-event-api.service.ts
@@ -27,7 +27,7 @@ export class MockTrainingEventApiService {
   /**
    * Default next five 'upcoming' events in order from soonest to latest.
    *
-   * @param {number} [numEvents=5]
+   * @param {number} [numEvents=5] Default value five upcoming events.
    * @returns {TrainingEvent[]}
    * @memberof MockTrainingEventApiService
    */
@@ -37,14 +37,38 @@ export class MockTrainingEventApiService {
     return seedTestData;
   }
 
-  sortEventsDateTime() {
+
+  /**
+   *
+   * @example "2019-08-04T00:30:39.038Z" "2019-02-02T03:47:07.045Z"
+   * @param {TrainingEvent[]} events
+   * @memberof MockTrainingEventApiService
+   */
+  sortEventsDateTime(events: TrainingEvent[]) {
     // use Array.sort() with custom function
+    // ISO 8601 can be sorted lexicographically. Or use Date
+
   }
 
+
+  /**
+   * Comparison function to get the future event that is happening soonest.
+   * The future dates that have least values are closest to the current date.
+   * Assumed.
+   *
+   * @memberof MockTrainingEventApiService
+   */
   compareUpcomingEventSoonest() {
 
   }
 
+
+  /**
+   * Comparison that gets the future event that is most distant from now.
+   * The future dates with larger values are further away from current date.
+   *
+   * @memberof MockTrainingEventApiService
+   */
   compareUpcomingEventLatest() {
 
   }
