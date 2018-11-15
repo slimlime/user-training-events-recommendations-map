5981fde9f1c084bf7da4384d3c61c3ffd0627a65
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-16 05:07:52 +1000

    :white_check_mark: Karma unit testing Resolved event list item input test failure

--- src/app/components/event-list-item/event-list-item.component.spec.ts
+++ src/app/components/event-list-item/event-list-item.component.spec.ts
@@ -1,14 +1,31 @@
+import { TrainingEvent } from './../../models/training-event';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { EventListItemComponent } from './event-list-item.component';
 
 describe('EventListItemComponent', () => {
   let component: EventListItemComponent;
   let fixture: ComponentFixture<EventListItemComponent>;
+  const mockTrainingEvent: TrainingEvent = {
+    id: 1,
+    title: 'Event Title A',
+    dateTime: '2019-01-15T18:38:52.483Z',
+    imageUrl: 'http://via.placeholder.com/100x100',
+    availableSeats: 128,
+    locationCityName: 'Brisbane',
+    locationCoordinates: [-27.4697707, 153.025123],
+    description: 'description text'
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EventListItemComponent ]
+      declarations: [ EventListItemComponent ],
+      providers: [
+        {
+          provide: Router, useValue: { }
+        }
+      ]
     })
     .compileComponents();
   }));
@@ -16,6 +33,7 @@ describe('EventListItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EventListItemComponent);
     component = fixture.componentInstance;
+    component.trainingEvent = mockTrainingEvent;
     fixture.detectChanges();
   });
 
