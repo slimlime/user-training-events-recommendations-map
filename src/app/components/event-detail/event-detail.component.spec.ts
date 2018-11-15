f561ca12e6e5c188415d4d9b840283ce2d1232d7
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-16 05:03:22 +1000

    Event detail Unit test mock setup

--- src/app/components/event-detail/event-detail.component.spec.ts
+++ src/app/components/event-detail/event-detail.component.spec.ts
@@ -1,14 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
+import { MockTrainingEventApiService } from '../../services/mock-training-event-api.service';
 import { EventDetailComponent } from './event-detail.component';
 
 describe('EventDetailComponent', () => {
   let component: EventDetailComponent;
   let fixture: ComponentFixture<EventDetailComponent>;
+  // const mockActivatedRoute = {
+  //   snapshot: {
+  //     data: {  },
+  //     paramMap: {  },
+  //     params: of({id: 1}),
+  //     url: 'event'
+  //   }
+  // } as ActivatedRoute;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EventDetailComponent ]
+      declarations: [ EventDetailComponent ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({id: 123}),
+            paramMap: of({ get: (id) => '1'}),
+            snapshot: {
+              paramMap: { get: (id) => '1'}
+            }
+          }
+        },
+        MockTrainingEventApiService
+      ]
     })
     .compileComponents();
   }));
