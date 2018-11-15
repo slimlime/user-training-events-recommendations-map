92e3e2a7c22bbd563b21ed96b6091979f1e39583
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-16 05:24:57 +1000

    :wrench::fire: Config tests mock

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -14,8 +14,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('http://maps.googleapis.com/maps/api/js?sensor=false&language=en')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
