9542e2f290a61238adcde2b27080bb54570f2480
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-16 01:28:39 +1000

    Get user location functionality

--- src/app/services/geolocation.service.ts
+++ src/app/services/geolocation.service.ts
@@ -8,10 +8,29 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GeolocationService {
+  userLocation = {
+    latitude: -27.1,
+    longitude: 153
+  };
 
   constructor() { }
 
+  /**
+   * Get user location from standard HTML5 Geolocation API
+   *
+   * @memberof GeolocationService
+   */
+  getUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position: Position) => {
+        console.log('​GeolocationService:: getUserLocation() -> position', position);
 
+        // Updates service userLocation
+        this.userLocation = position.coords;
+      });
+    }
+    return this.userLocation;
+  }
   /**
    * Helper utility function to convert degrees to radians
    * @param {number} degrees
