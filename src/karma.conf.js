413c30527589ba7ac6ef8d7e03c62d747e27fe5c
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-16 05:17:38 +1000

    :wrench: Config tests mock

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,11 +1,12 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-files = [
-  'src/app/mocks/google-maps-mock.js'
-]
+
 module.exports = function (config) {
   config.set({
     basePath: '',
+    files = [
+      'src/app/mocks/google-maps-mock.js'
+    ],
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
