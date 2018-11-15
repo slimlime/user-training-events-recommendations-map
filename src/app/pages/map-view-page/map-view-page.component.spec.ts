fe56ebc95a430bcb25a6a6134da7dc17b0e0f204
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-16 05:37:22 +1000

    :fire::white_check_mark: Strange Google Maps mocking. Defer for later

--- src/app/pages/map-view-page/map-view-page.component.spec.ts
+++ src/app/pages/map-view-page/map-view-page.component.spec.ts
@@ -1,17 +1,122 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapViewPageComponent } from './map-view-page.component';
+import { InjectionToken } from '@angular/core';
 // Need mock-google maps
 // declare let google: any;
 require('src/app/mocks/google-maps-mock');
+export const GOOGLE = new InjectionToken('google');
+export const googleFactory = () => google;
 
+const mockedGoogle = {
+  google: {
+    maps: {
+      Animation: {},
+      BicyclingLayer: function () { },
+      Circle: function () { },
+      ControlPosition: {},
+      Data: function () { },
+      DirectionsRenderer: function () { },
+      DirectionsService: function () { },
+      DirectionsStatus: {},
+      DirectionsTravelMode: {},
+      DirectionsUnitSystem: {},
+      DistanceMatrixElementStatus: {},
+      DistanceMatrixService: function () { },
+      DistanceMatrixStatus: {},
+      ElevationService: function () { },
+      ElevationStatus: {},
+      FusionTablesLayer: function () { },
+      Geocoder: function () { },
+      GeocoderLocationType: {},
+      GeocoderStatus: {},
+      GroundOverlay: function () { },
+      ImageMapType: function () { },
+      InfoWindow: function () { },
+      KmlLayer: function () { },
+      KmlLayerStatus: {},
+      LatLng: function () { },
+      LatLngBounds: function () { },
+      MVCArray: function () { },
+      MVCObject: function () { },
+      Map: function () {
+        return {
+          setTilt: function () { },
+          mapTypes: {
+            set: function () { }
+          },
+          overlayMapTypes: {
+            insertAt: function () { },
+            removeAt: function () { }
+          }
+        };
+      },
+      MapTypeControlStyle: {},
+      MapTypeId: {
+        HYBRID: '',
+        ROADMAP: '',
+        SATELLITE: '',
+        TERRAIN: ''
+      },
+      MapTypeRegistry: function () { },
+      Marker: function () { },
+      MarkerImage: function () { },
+      MaxZoomService: function () {
+        return {
+          getMaxZoomAtLatLng: function () { }
+        };
+      },
+      MaxZoomStatus: {},
+      NavigationControlStyle: {},
+      OverlayView: function () { },
+      Point: function () { },
+      Polygon: function () { },
+      Polyline: function () { },
+      Rectangle: function () { },
+      SaveWidget: function () { },
+      ScaleControlStyle: {},
+      Size: function () { },
+      StreetViewCoverageLayer: function () { },
+      StreetViewPanorama: function () { },
+      StreetViewService: function () { },
+      StreetViewStatus: {},
+      StrokePosition: {},
+      StyledMapType: function () { },
+      SymbolPath: {},
+      TrafficLayer: function () { },
+      TransitLayer: function () { },
+      TransitMode: {},
+      TransitRoutePreference: {},
+      TravelMode: {},
+      UnitSystem: {},
+      ZoomControlStyle: {},
+      __gjsload__: function () { },
+      event: {
+        addListener: function () { }
+      },
+      places: {
+        AutocompleteService: function () {
+          return {
+            getPlacePredictions: function () { }
+          };
+        }
+      }
+    }
+  }
+};
 describe('MapViewPageComponent', () => {
   let component: MapViewPageComponent;
   let fixture: ComponentFixture<MapViewPageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapViewPageComponent ]
+      declarations: [ MapViewPageComponent ],
+      providers: [
+        {
+          provide: GOOGLE,
+          useFactory: googleFactory
+        }
+      ]
     })
     .compileComponents();
   }));
@@ -26,3 +131,4 @@ describe('MapViewPageComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
