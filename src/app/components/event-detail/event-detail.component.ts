66bddda2b2990f239a8077dbd8492f58ec1a7336
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-16 04:06:38 +1000

    Setup event detail component a bit smartcomponent to get event information on direct links.

--- src/app/components/event-detail/event-detail.component.ts
+++ src/app/components/event-detail/event-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { TrainingEvent } from '../../models/training-event';
+import { MockTrainingEventApiService } from './../../services/mock-training-event-api.service';
 
 @Component({
   selector: 'app-event-detail',
@@ -9,10 +11,26 @@ import { TrainingEvent } from '../../models/training-event';
 })
 export class EventDetailComponent implements OnInit {
   @Input() trainingEvent: TrainingEvent;
-  constructor() { }
+  constructor(
+    public activatedRoute: ActivatedRoute,
+    public trainingEventApiService: MockTrainingEventApiService
+  ) {
+
+  }
 
   ngOnInit() {
 
   }
 
+  getEventInformation(
+    activatedRoute: ActivatedRoute,
+    eventApi: MockTrainingEventApiService
+  ) {
+    // Get parameter string id and convert to number.
+    const eventIDParam = activatedRoute.snapshot.paramMap.get('id');
+    const eventID: number = +eventIDParam;
+    // Get event information for the given id.
+    eventApi.getEventByID(+eventID);
+
+  }
 }
