e21a61ebe74be0e7ae9dff624578b39f1fb4305c
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-11-15 10:29:40 +1000

    eg Geo coordinates distance formula

--- src/app/services/geolocation.service.ts
+++ src/app/services/geolocation.service.ts
@@ -6,4 +6,27 @@ import { Injectable } from '@angular/core';
 export class GeolocationService {
 
   constructor() { }
+
+  degreesToRadians(degrees) {
+    const radians = degrees * (Math.PI / 180);
+    return radians;
+  }
+
+
+  getDistanceKilometresBetweenCoordinates(pt1, pt2) {
+    const R = 6371; // Radius of the earth in km
+    const diffLat = this.degreesToRadians(pt2.latitude - pt1.latitude);
+    const diffLon = this.degreesToRadians(pt2.longitude - pt1.longitude);
+    const a = Math.sin(diffLat / 2) * Math.sin(diffLat / 2) +
+      Math.cos(this.degreesToRadians(pt1.latitude)) *
+      Math.cos(this.degreesToRadians(pt2.latitude)) *
+      Math.sin(diffLon / 2) * Math.sin(diffLon / 2)
+      ;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
+
+    return d;
+  }
+
+
 }
